b0ec7cadb972532f12af2b8be636608da91e4504
Phi Nam <phinamhd1992@gmail.com>
2019-10-28 16:48:08 +0700

    ok

---
+++ src/displayHello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class displayHello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap ten");
+        String name = scanner.nextLine();
+        System.out.println("Xin chao "+name);
+    }
+}
